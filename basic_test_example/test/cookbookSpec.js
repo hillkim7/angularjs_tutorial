02db3f5c47c0d983859caa03481d4750b96b64b1
Hill Kim <flowtide@naver.com>
2015-07-08 14:45:21 +0900

    Running a simple test using Jasmine

---
+++ basic_test_example/test/cookbookSpec.js
@@ -0,0 +1,14 @@
+describe('MainCtrl', function() {
+  beforeEach(module('cookbook'));
+  it('should assign the correct rapper to scope',
+  inject(function ($controller, $rootScope) {
+    var $scope = $rootScope.$new();
+    $controller('MainCtrl', {
+      $scope: $scope
+    });
+
+    expect($scope.emcee).toEqual('Kool G Rap');
+  }));
+
+});
+
